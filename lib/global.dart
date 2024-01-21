95bed37aa192661f60fa57484e9df4e5fddbf71e
IliaLataria01 <ilataria@solvd.com>
2024-01-22 02:40:01 +0400

    Added initialization of firbase at the begining

--- lib/global.dart
+++ lib/global.dart
@@ -1,10 +1,15 @@
 import 'package:chatty/common/services/services.dart';
 import 'package:chatty/common/store/store.dart';
+import 'package:chatty/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
 class Global {
   static Future init() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform
+    );
     WidgetsFlutterBinding.ensureInitialized();
     await Get.putAsync<StorageService>(() => StorageService().init());
     Get.put<UserStore>(UserStore());
